7bbdce763093b7a4868ad9012da5db8be4d81ae0
k-lawl <70022050+lawless92@users.noreply.github.com>
2026-09-15 17:33:29 +0200

    Refactor backup script to use empty path variables

--- copy-backup.ps1
+++ copy-backup.ps1
@@ -1,6 +1,6 @@
 
-$RutaLocal = "E:\BKSQL"
-$RutaRemota = "\\ESTACNS01APP\E$\BKSQL"
+$RutaLocal = ""
+$RutaRemota = ""
 
 $BackupReciente = Get-ChildItem  -Path $RutaLocal -FIlter *.bak | Sort-Object LastWriteTime -Descending | Select-Object -First 3
 
